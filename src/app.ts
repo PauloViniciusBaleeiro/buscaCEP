d32a315bc2bc858d599fe0a865f98ed08e3428fd
paulo <pauloviniciusbf@gmail.com>
2021-01-25 23:02:08 -0300

    feat: Adição de gerenciador para rotas inexistentes

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
-import express from 'express';
-import expressValidator from 'express-validator';
+import dotenv from 'dotenv';
+import express, {RequestHandler} from 'express';
+// import expressValidator from 'express-validator';
 import healthcheck from './controllers/healthcheck.controller';
-import controllersV1 from './controllers/v1/index.controller';
-import * as swaggerDoc from './swagger.json';
+// import controllersV1 from './controllers/v1/index.controller';
+// import * as swaggerDoc from './swagger.json';
 import swaggerUI from 'swagger-ui-express';
 
+
+dotenv.config();
 
 // Criação da aplicação
 const app = express();
@@ -13,22 +16,33 @@ const app = express();
 app.use(express.json());
 
 // express validator
-app.use(expressValidator());
+// app.use(expressValidator());
 
-app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerDoc));
+// app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerDoc));
 
-app.use('/healthcheck', cors(), healthcheck);
-app.use('/v1', controllersV1);
+app.use('/healthcheck', healthcheck);
+// app.use('/v1', controllersV1);
+
+
+function resourceNotFound() {
+  const http404: RequestHandler = (req, res, next) => {
+    res.status(404).json('Recurso não encontrado');
+  }
+  return http404;
+}
+
+app.use(resourceNotFound());
+  
 
 const server = app.listen(process.env.PORT, async () => {
-    try {
-      null;
-    } catch (err) {
-      console.log('Oops, ocorreu um erro para se conectar ao Postgre', err);
-    }
-    console.log('Express server rodando na porta %d', process.env.PORT);
-    app.emit(`app_started`);
-  });
-  
-  export default server;
+  try {
+    null;
+  } catch (err) {
+    console.log('Oops, ocorreu um erro para se conectar ao Postgre', err);
+  }
+  console.log('Express server rodando na porta %d', process.env.PORT);
+  app.emit(`app_started`);
+});
+
+export default server;
 
