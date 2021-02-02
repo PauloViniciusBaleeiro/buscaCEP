62aa3729590d166994378a844fb1143fc9d4b50a
paulo <pauloviniciusbf@gmail.com>
2021-02-01 23:22:58 -0300

    feat: Adição inicial de arquivo swagger

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,8 @@
 import dotenv from 'dotenv';
 import express, {RequestHandler} from 'express';
-// import expressValidator from 'express-validator';
 import healthcheck from './controllers/healthcheck.controller';
-// import controllersV1 from './controllers/v1/index.controller';
-// import * as swaggerDoc from './swagger.json';
+import controllersV1 from './controllers/v1/index.controller';
+import * as swaggerDoc from './swagger.json';
 import swaggerUI from 'swagger-ui-express';
 
 
@@ -15,13 +14,11 @@ const app = express();
 // 
 app.use(express.json());
 
-// express validator
-// app.use(expressValidator());
 
-// app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerDoc));
+app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerDoc));
 
 app.use('/healthcheck', healthcheck);
-// app.use('/v1', controllersV1);
+app.use('/v1', controllersV1);
 
 
 function resourceNotFound() {
